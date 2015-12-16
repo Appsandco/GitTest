e511fa71ebfb5e39f4be1c51f5b9bdfe002ea201
Apps Co <James@imac-de-apps>
2015-12-16 11:22:14 +0100

    Premier commit d'une nouvelle branche...

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -14,6 +14,8 @@
 
 @property (nonatomic, strong) TestClass *testClass;
 
+-(void)sayHello;
+
 @end
 
 @implementation ViewController
@@ -31,11 +33,17 @@
     self.sum = a + b;
     
     NSLog(@"The result is: %d", self.sum);
+    
+    [self sayHello];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)sayHello{
+    NSLog(@"Hello");
 }
 
 @end
